8859052d632590b1f49b59e42fdbc82b02ced42a
Diana Le <dianapnle@gmail.com>
2024-07-30 01:00:24 -0700

    added validation for signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,13 +3,60 @@ const express = require('express')
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 
 const router = express.Router();
 
 
+const validateSignup = [
+    //It checks to see if req.body.email exists and is an email
+    check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('The provided email is invalid.')
+    .custom(async value => {
+      const user = await User.findAll( {where: {email: value}})
+      if (user.length !== 0) {
+        throw new Error('Email must be unique');
+      }
+  }),
+    //checks if req.body.username is a minimum length of 4 and is not an email
+    check('username')
+    .custom(async value => {
+        const user = await User.findAll( {where: {username: value}} )
+        if (user.length !== 0) {
+          throw new Error('Username must be unique');
+        }
+    })
+    .exists({ checkFalsy: true })
+    .withMessage('Username required')
+    .isLength({ min: 4 })
+    .withMessage('Username needs to be at least 4 characters'),
+    check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+    //checks if req.body.password is not empty and has a minimum length of 6
+    check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('First Name is required'),
+    check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Last Name is required'),
+    handleValidationErrors
+]
+
+
+
+
 // Sign up
-router.post('/', async (req, res) => {
+router.post('/', validateSignup, async (req, res) => {
     const { email, password, firstName, lastName, username } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({ email, username, hashedPassword, firstName, lastName });
