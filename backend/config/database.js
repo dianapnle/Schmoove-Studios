0274426536a3f61e9b69c3c1f0ebee4a7f942dbc
Diana Le <dianapnle@gmail.com>
2024-09-03 13:17:34 -0700

    removed seeder storage

--- backend/config/database.js
+++ backend/config/database.js
@@ -12,7 +12,6 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    seederStorage: 'sequelize',
     dialectOptions: {
       ssl: {
         require: true,
